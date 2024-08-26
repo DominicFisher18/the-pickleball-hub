167146639161eeaaf1acb3bd9606229bd7541e94
DomDom <dominicfisher182001@gmail.com>
2024-08-26 11:53:37 -0400

    finished add to cart functionality

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,12 +1,36 @@
 let cartBtn = document.querySelectorAll('.add-to-cart')
 
 cartBtn.forEach((button) => {
-    button.addEventListener('click', addToCart)
+    button.addEventListener('click', addToCartButtonClick)
 })
 
-function addToCart(event) {
+function addToCartButtonClick(event) {
     let button = event.target
-    let itemDiv = button.parentElement.parentElement
-    let itemName = itemDiv.document.querySelectorAll('.item-name')[0].innerText
-    console.log(itemName)
+    let firstDiv = button.parentElement.parentElement
+    let secondDiv = button.parentElement
+    // console.log(itemDiv.firstElementChild)
+    let itemName = firstDiv.firstElementChild.innerHTML
+    let itemPrice = secondDiv.firstElementChild.innerHTML
+    console.log(itemName, itemPrice)
+    addToCart(itemName, itemPrice)
+}
+
+function addToCart(itemName, itemPrice) {
+    let cartDiv = document.querySelector('.js-cart-div')
+    let cartRow = document.createElement('div')
+    let cartRowInfo = `
+        <div class="cart-row">
+            <div class="name-and-price">
+                <div class="cart-name">${itemName}</div>
+                <div class="cart-price">${itemPrice}</div>
+            </div>
+
+            <div class="delete-and-quantity">
+                <button class="delete-button">Delete</button>
+            </div>
+        </div>
+    `
+
+    cartRow.innerHTML = cartRowInfo
+    cartDiv.append(cartRow)
 }
